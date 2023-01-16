f5bd10a613073faea27c3e324a020b0f245e9e10
Pedro <pedro.sacramento-xavier-barreto-rosa@student-cs.fr>
2023-01-16 16:52:23 +0100

    fixed serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,14 @@ from main.models import Alarm
 from rest_framework import serializers
 import json
 
+class ModeSerializerField(serializers.Field):
+    def to_representation(self, value):
+        return eval(value)
+
 class AlarmsSerializer(serializers.ModelSerializer):
+
+    mode = ModeSerializerField()
+
     class Meta:
         model = Alarm
         fields = "__all__"
-        
